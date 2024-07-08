765439801a913b039f539682386a0f258a4be534
Issa Beekun <goodflyerfye@gmail.com>
2024-07-08 15:45:29 -0700

    Add test that will fail to test commit protections

--- src/test/java/com/battre/gwsvc/GwSvcApplicationTests.java
+++ src/test/java/com/battre/gwsvc/GwSvcApplicationTests.java
@@ -2,9 +2,15 @@ package com.battre.gwsvc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class GwSvcApplicationTests {
   @Test
   void contextLoads() {}
+
+  @Test
+  void alwaysFails() {
+    fail("This test is designed to always fail.");
+  }
 }
